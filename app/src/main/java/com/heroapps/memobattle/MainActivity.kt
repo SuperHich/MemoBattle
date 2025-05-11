de619df7a51a5732cfc9ba0346045e5b45525d84
h.laroussi <h.laroussi@oodrive.com>
2025-05-11 23:50:59 +0200

    Remove GreetingPreview composable

--- app/src/main/java/com/heroapps/memobattle/MainActivity.kt
+++ app/src/main/java/com/heroapps/memobattle/MainActivity.kt
@@ -6,9 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.google.android.gms.ads.MobileAds
 import com.heroapps.memobattle.ui.theme.MemoBattleTheme
 
@@ -29,13 +27,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MemoBattleTheme {
-        MemoryGameApp()
     }
 }
