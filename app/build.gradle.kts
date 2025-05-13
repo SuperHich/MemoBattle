54fc0695755aecbe5dd37b5376624ea4dd656179
h.laroussi <h.laroussi@oodrive.com>
2025-05-13 22:15:40 +0200

    Upgrade version to 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = dateAsString().toInt()
-        versionName = "1.0"
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
